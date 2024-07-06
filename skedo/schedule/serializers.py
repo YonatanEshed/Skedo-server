9f44869adbdd8b7dfc58bd10aa89c7da87b5ca43
Yonatan Eshed <yonatan.eshed@gmail.com>
2024-07-06 10:48:47 +0300

    feat(serializer): switch to ModelSerializer

--- skedo/schedule/serializers.py
+++ skedo/schedule/serializers.py
@@ -1,33 +1,35 @@
-from rest_framework.serializers import Serializer
+from rest_framework.serializers import ModelSerializer, IntegerField
 
 from .models import Schedule, Group, GroupActivity
 
 
-class ScheduleSerializer(Serializer):
+class ScheduleSerializer(ModelSerializer):
+    id = IntegerField(source='pk', read_only=True)
+
     class Meta:
         model = Schedule
         fields = "__all__"
-        read_only_fields = (
+        read_only_fields = [
             "user"
-        )
+        ]
 
 
-class GroupSerializer(Serializer):
+class GroupSerializer(ModelSerializer):
     class Meta:
         model = Group
         fields = "__all__"
-        read_only_fields = (
+        read_only_fields = [
             "user"
-        )
+        ]
 
 
-class GroupActivitySerializer(Serializer):
+class GroupActivitySerializer(ModelSerializer):
     group = GroupSerializer()
 
     class Meta:
         model = GroupActivity
         fields = "__all__"
-        read_only_fields = (
+        read_only_fields = [
             "group",
             "schedule",
-        )
+        ]
