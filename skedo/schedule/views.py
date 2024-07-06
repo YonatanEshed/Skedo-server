b7b8c4a2df2a4d651ee6443e73aeca8406fca24a
Yonatan Eshed <yonatan.eshed@gmail.com>
2024-07-06 10:57:58 +0300

    feat(views): add schedule views

--- skedo/schedule/views.py
+++ skedo/schedule/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import ScheduleSerializer
+from .models import Schedule
+
+
+class ScheduleView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            schedule = Schedule.objects.get(user=request.user)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ScheduleSerializer(schedule)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request):
+        try:
+            schedule = Schedule.objects.get(user=request.user)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ScheduleSerializer(schedule, data=request.data)
+        if (serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
