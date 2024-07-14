1319ef158286cf1d7ca380d9163304ba3d776831
Yonatan Eshed <yonatan.eshed@gmail.com>
2024-07-15 01:40:20 +0300

    feat: add urls for group views

--- skedo/schedule/urls.py
+++ skedo/schedule/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import ScheduleView
+from .views import ScheduleView, GroupView, GroupDetailView
 
 urlpatterns = [
     path("schedule/", ScheduleView.as_view(), name="get-put-schedule"),
+    path("group/", GroupView.as_view(), name="get-post-group"),
+    path("group/<int:pk>", GroupDetailView.as_view(), name="get-put-delete-group"),
 ]
